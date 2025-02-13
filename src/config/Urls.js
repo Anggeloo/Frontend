295d332d5ea32190408398d0f1303115bbcb1092
PamelaCaiza11 <3a.caiza.pamela@gmail.com>
2025-02-13 10:18:03 -0500

    Adding frontend files

---
+++ src/config/Urls.js
@@ -0,0 +1,40 @@
+export const urlAuthentication = 'http://localhost:5201/Authentication' // es la url del servicio de authenticacion
+export const urlSocket  = 'http://localhost:5201/workerhub' // es la url del servicio de authenticacion
+
+export const urlHistory = 'http://localhost:3010/graphql' // es la url del servicio de history
+
+export const urlProduct = {
+    search : 'http://localhost:5050/products',
+    create : 'http://localhost:5050/products',
+    update : 'http://localhost:5050/products',
+    delete : 'http://localhost:5050/products',
+}
+
+export const urlInventory = {
+    search : 'http://localhost:8080/inventory',
+    create : 'http://localhost:8081/inventory',
+    update : 'http://localhost:8082/inventory',
+    delete : 'http://localhost:8083/inventory',
+}
+
+export const urlWorkTeam = {
+    search : 'http://localhost:8180/work-team',
+    create : 'http://localhost:8181/work-team',
+    update : 'http://localhost:8182/work-team',
+    delete : 'http://localhost:8183/work-team',
+}
+
+export const urlOrder = {
+    search : 'http://localhost:5050/Orders',
+    create : 'http://localhost:5050/Orders',
+    update : 'http://localhost:5050/Orders',
+    delete : 'http://localhost:5050/Orders',
+}
+
+export const urlTeamPerformance =  'http://localhost:5050/TeamPerformanceAnalysis'
+
+export const urlRawMaterialManagement = 'http://localhost:5050/RawMaterialManagement'
+
+export const urlMonthlySummary = 'http://localhost:5050/MonthlySummary'
+
+export const urlInformationGraphics = 'http://localhost:5050/InformationGraphic'
